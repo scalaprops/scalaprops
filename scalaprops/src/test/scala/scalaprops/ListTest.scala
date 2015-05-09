1d5f48f13a9bca610fce922fe6d1a950128f6de8
xuwei-k <6b656e6a69@gmail.com>
2015-05-09 12:11:42 +0900

    ListTest

--- scalaprops/src/test/scala/scalaprops/ListTest.scala
+++ scalaprops/src/test/scala/scalaprops/ListTest.scala
@@ -9,6 +9,10 @@ object ListTest extends Scalaprops {
   val testLaws = Properties.either(
     "List laws",
     scalazlaws.monadPlus.all[List],
+    scalazlaws.align.all[List],
+    scalazlaws.zip.all[List],
+    scalazlaws.isEmpty.all[List],
+    scalazlaws.cobind.all[List],
     scalazlaws.traverse.all[List]
   )
 
