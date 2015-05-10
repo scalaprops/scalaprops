027d7eefa7b98a52019e206ac3231a14d61939c0
xuwei-k <6b656e6a69@gmail.com>
2015-05-11 00:47:35 +0900

    more OptionT test

--- scalaprops/src/test/scala/scalaprops/OptionTTest.scala
+++ scalaprops/src/test/scala/scalaprops/OptionTTest.scala
@@ -1,12 +1,30 @@
 package scalaprops
 
 import scalaz._
+import scalaz.std.string._
 import scalaz.std.anyVal._
 import scalaz.std.option._
 
 object OptionTTest extends Scalaprops{
-  val testIList =
-    scalazlaws.monadPlus.all[({type l[a] = OptionT[IList, a]})#l]
-  val testMaybe =
-    scalazlaws.monadPlus.all[({type l[a] = OptionT[Maybe, a]})#l]
+
+  val iList = {
+    type F[A] = OptionT[IList, A]
+    Properties.either(
+      "OptionT[IList, _]",
+      scalazlaws.equal.all[OptionT[IList, Int]],
+      scalazlaws.traverse.all[F],
+      scalazlaws.monadPlus.all[F]
+    )
+  }
+
+  val maybe = {
+    type F[A] = OptionT[Maybe, A]
+    Properties.either(
+      "OptionT[Maybe, _]",
+      scalazlaws.equal.all[OptionT[Maybe, Int]],
+      scalazlaws.traverse.all[F],
+      scalazlaws.monadPlus.all[F]
+    )
+  }
+
 }
