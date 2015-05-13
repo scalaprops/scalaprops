f3afbed2296ff74253cc9390a1b882da1fbb31c4
xuwei-k <6b656e6a69@gmail.com>
2015-05-14 01:25:33 +0900

    TreeTest

---
+++ scalaprops/src/test/scala/scalaprops/TreeTest.scala
@@ -0,0 +1,33 @@
+package scalaprops
+
+import scalaz._
+import scalaz.std.anyVal._
+import scalaz.std.string._
+
+object TreeTest extends Scalaprops {
+
+  val traverse1 = scalazlaws.traverse1.all[Tree]
+  val monad = scalazlaws.monad.all[Tree]
+  val equal = scalazlaws.equal.all[Tree[Int]]
+
+  // TODO test comonad law
+
+  val treeGenSize = {
+    val F = Foldable[Tree]
+    val p = { (size: Int) =>
+      Property.forAll{ tree: Tree[Int] =>
+        val c = F.count(tree)
+        (c <= size) && ((c * 0.7) < F.toIList(tree).distinct.length)
+      }.toProperties(size.toString).andThenParam(
+        Param.maxSize(size)
+      )
+    }
+
+    Properties.fromProps(
+      "Gen[Tree]",
+      p(10),
+      List.iterate(100, 2)(_ * 10).map(p): _*
+    )
+  }
+
+}
