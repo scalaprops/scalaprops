1030aa855ca73c766967a339796794b34295dd9a
xuwei-k <6b656e6a69@gmail.com>
2015-05-15 04:00:29 +0900

    add *^*->*.L, R, Empty, T

--- scalazlaws/src/main/scala/scalaprops/scalazlaws/kinds.scala
+++ scalazlaws/src/main/scala/scalaprops/scalazlaws/kinds.scala
@@ -7,8 +7,15 @@ import scalaz.std.string._
 final class *^*->* private(override val toString: String) {}
 
 object *^*->* {
+
+  type T = *^*->* :-: Unit :-: Or.Empty
+
   val left = new *^*->*("left")
   val right = new *^*->*("right")
+
+  val L: T = Or[T](left)
+  val R: T = Or[T](right)
+  val Empty: T = Or[T](())
 
   implicit val instance: Order[*^*->*] =
     Order.orderBy(_.toString)
