cfea8f6d34c60f1586e8bb56c28e41b9ba63e166
xuwei-k <6b656e6a69@gmail.com>
2015-05-19 10:34:40 +0900

    PartialFunctionTest

---
+++ scalaprops/src/test/scala/scalaprops/PartialFunctionTest.scala
@@ -0,0 +1,17 @@
+package scalaprops
+
+import scalaz._
+import scalaz.std.partialFunction._
+import scalaz.std.option._
+import scalaz.std.anyVal._
+import scalaz.std.tuple._
+import FunctionEqual._
+
+object PartialFunctionTest extends Scalaprops {
+
+  private[this] implicit def equal[A: Gen, B: Equal]: Equal[PartialFunction[A, B]] =
+    Equal[A => Option[B]].contramap(_.lift)
+
+  val law = scalazlaws.arrow.all[PartialFunction]
+
+}
