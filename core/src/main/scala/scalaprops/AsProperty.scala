18027fca14208e48b975a4d6e2b054ea10fc221a
xuwei-k <6b656e6a69@gmail.com>
2015-05-25 23:12:08 +0900

    add AsProperty

---
+++ core/src/main/scala/scalaprops/AsProperty.scala
@@ -0,0 +1,38 @@
+package scalaprops
+
+import scalaz.Contravariant
+
+/**
+ * @see [[scalaprops.Bool]]
+ */
+abstract class AsProperty[A] { self =>
+  def asProperty(a: A): Property
+  def toFunction: A => Property = asProperty
+  final def contramap[B](f: B => A): AsProperty[B] =
+    new AsProperty[B] {
+      def asProperty(b: B) = self.asProperty(f(b))
+    }
+}
+
+object AsProperty {
+  def from[A](f: A => Property): AsProperty[A] =
+    new AsProperty[A] {
+      override val toFunction = f
+      def asProperty(a: A) = f(a)
+    }
+
+  implicit val bool: AsProperty[Bool] =
+    from(b => Property.prop(b.b))
+
+  implicit val boolean: AsProperty[Boolean] =
+    from(Property.prop)
+
+  implicit val property: AsProperty[Property] =
+    from(identity)
+
+  implicit val instance: Contravariant[AsProperty] =
+    new Contravariant[AsProperty] {
+      def contramap[A, B](r: AsProperty[A])(f: B => A) =
+        r contramap f
+    }
+}
