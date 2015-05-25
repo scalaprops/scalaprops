0fd21dead0e20813f827717f1c47b17a9aa1953c
xuwei-k <6b656e6a69@gmail.com>
2015-05-26 03:21:07 +0900

    Scalaprops.filterUnitEmpty

--- scalaprops/src/main/scala/scalaprops/Scalaprops.scala
+++ scalaprops/src/main/scala/scalaprops/Scalaprops.scala
@@ -1,5 +1,7 @@
 package scalaprops
 
+import scalaz._
+
 trait Scalaprops {
 
   def param: Param = Param.withCurrentTimeSeed()
@@ -8,6 +10,21 @@ trait Scalaprops {
     ScalapropsListener.default
 
   def transformProperties[A](properties: List[Properties[A]]): List[Properties[A]] =
-    properties.sortBy(_.id.toString)
+    properties.map(Scalaprops.filterUnitEmpty).sortBy(_.id.toString)
+
+}
+
+object Scalaprops {
+
+  def filterUnitEmpty[A](p: Properties[A]): Properties[A] = {
+    def loop(tree: Tree[(A, Maybe[Check])]): Tree[(A, Maybe[Check])] =
+      tree match {
+        case Tree.Node(root, Stream(Tree.Node((Or.L(()), Maybe.Empty()), sub))) =>
+          Tree.node(root, sub.map(loop))
+        case _ =>
+          Tree.node(tree.rootLabel, tree.subForest.map(loop))
+      }
+    Properties.noSort(loop(p.props))
+  }
 
 }
