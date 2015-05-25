eb14e5ea2ed1a037162f97cf35983b0c21a62efb
xuwei-k <6b656e6a69@gmail.com>
2015-05-25 23:13:42 +0900

    add test for Bool#implies

--- scalaprops/src/test/scala/scalaprops/PropertyTest.scala
+++ scalaprops/src/test/scala/scalaprops/PropertyTest.scala
@@ -15,4 +15,13 @@ object PropertyTest extends Scalaprops{
     sideEffect
   }
 
+  val `Bool#implies is lazy` = Property.forAll{
+    val f = Bool.bool(false)
+    var flag = true
+    def sideEffect(): Unit = flag = false
+    assert(f.implies[Boolean]{ sideEffect(); true}.f(100, param.rand)._1.isNoResult)
+    assert(f.implies[Bool]{ sideEffect(); f}.f(100, param.rand)._1.isNoResult)
+    assert(f.implies[Property]{ sideEffect(); Property.prop(true)}.f(100, param.rand)._1.isNoResult)
+    flag
+  }
 }
