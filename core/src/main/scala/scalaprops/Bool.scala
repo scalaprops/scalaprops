5d43dc8587509dd8c9d71433a388ee4e48397957
xuwei-k <6b656e6a69@gmail.com>
2015-05-25 23:12:30 +0900

    remove overloads. use AsProperty

--- core/src/main/scala/scalaprops/Bool.scala
+++ core/src/main/scala/scalaprops/Bool.scala
@@ -3,12 +3,8 @@ package scalaprops
 final case class Bool private(b: Boolean) {
   def is = b
   def isNot = !is
-  def implies(p: => Property): Property =
-    Property.implies(b, p)
-  def implies(c: Bool): Property =
-    implies(c.b)
-  def implies(c: Boolean): Property =
-    implies(Property.prop(b))
+  def implies[A](p: => A)(implicit A: AsProperty[A]): Property =
+    Property.implies(b, A.asProperty(p))
 }
 
 object Bool {
