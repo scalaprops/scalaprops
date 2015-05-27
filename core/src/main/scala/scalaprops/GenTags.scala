8cc351d507d2b7961c9a22e9ac4758b0c5f22574
xuwei-k <6b656e6a69@gmail.com>
2015-05-28 02:52:29 +0900

    add some String and Char newtype instances

--- core/src/main/scala/scalaprops/GenTags.scala
+++ core/src/main/scala/scalaprops/GenTags.scala
@@ -1,25 +1,61 @@
 package scalaprops
 
-import scalaz.Tag
+import scalaz._
 
 object GenTags {
+
+  private[this] type CharInstance[A] =
+    Monoid[A] with Enum[A] with Show[A]
+
+  private[this] type StringInstance[A] =
+    Monoid[A] with Show[A] with Order[A] with IsEmpty[({type l[_] = A})#l]
 
   sealed trait Num
   val Num = Tag.of[Num]
 
+  implicit val numCharInstance: CharInstance[Char @@ Num] =
+    Num.subst[CharInstance, Char](scalaz.std.anyVal.char)
+  implicit val numStringInstance: StringInstance[String @@ Num] =
+    Num.subst[StringInstance, String](scalaz.std.string.stringInstance)
+
   sealed trait AlphaUpper
   val AlphaUpper = Tag.of[AlphaUpper]
+
+  implicit val alphaUpperCharInstance: CharInstance[Char @@ AlphaUpper] =
+    AlphaUpper.subst[CharInstance, Char](scalaz.std.anyVal.char)
+  implicit val alphaUpperStringInstance: StringInstance[String @@ AlphaUpper] =
+    AlphaUpper.subst[StringInstance, String](scalaz.std.string.stringInstance)
 
   sealed trait AlphaLower
   val AlphaLower = Tag.of[AlphaLower]
 
+  implicit val alphaLowerCharInstance: CharInstance[Char @@ AlphaLower] =
+    AlphaLower.subst[CharInstance, Char](scalaz.std.anyVal.char)
+  implicit val alphaLowerStringInstance: StringInstance[String @@ AlphaLower] =
+    AlphaLower.subst[StringInstance, String](scalaz.std.string.stringInstance)
+
   sealed trait Alpha
   val Alpha = Tag.of[Alpha]
+
+  implicit val alphaCharInstance: CharInstance[Char @@ Alpha] =
+    Alpha.subst[CharInstance, Char](scalaz.std.anyVal.char)
+  implicit val alphaStringInstance: StringInstance[String @@ Alpha] =
+    Alpha.subst[StringInstance, String](scalaz.std.string.stringInstance)
 
   sealed trait AlphaNum
   val AlphaNum = Tag.of[AlphaNum]
 
+  implicit val alphaNumCharInstance: CharInstance[Char @@ AlphaNum] =
+    AlphaNum.subst[CharInstance, Char](scalaz.std.anyVal.char)
+  implicit val alphaNumStringInstance: StringInstance[String @@ AlphaNum] =
+    AlphaNum.subst[StringInstance, String](scalaz.std.string.stringInstance)
+
   sealed trait Ascii
   val Ascii = Tag.of[Ascii]
+
+  implicit val asciiCharInstance: CharInstance[Char @@ Ascii] =
+    Ascii.subst[CharInstance, Char](scalaz.std.anyVal.char)
+  implicit val asciiStringInstance: StringInstance[String @@ Ascii] =
+    Ascii.subst[StringInstance, String](scalaz.std.string.stringInstance)
 
 }
