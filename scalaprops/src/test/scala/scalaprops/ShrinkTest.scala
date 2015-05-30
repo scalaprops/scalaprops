252f6415fa26ff15a46523dd1542d86b721783a5
xuwei-k <6b656e6a69@gmail.com>
2015-05-31 02:33:08 +0900

    Shrink test

--- scalaprops/src/test/scala/scalaprops/ShrinkTest.scala
+++ scalaprops/src/test/scala/scalaprops/ShrinkTest.scala
@@ -1,6 +1,7 @@
 package scalaprops
 
-import scalaz.Equal
+import java.util.concurrent.atomic.AtomicBoolean
+import scalaz._
 import scalaz.std.anyVal._
 import scalaz.std.stream._
 
@@ -13,4 +14,29 @@ object ShrinkTest extends Scalaprops {
 
   val law = scalazlaws.invariantFunctor.all[Shrink]
 
+  val int = {
+    val x = 128
+
+    Property.property{ seed: Int =>
+      val param0 = super.param.copy(
+        rand = Rand.fromSeed(seed)
+      )
+
+      val m = 6
+      val g = Gen.choose(List.fill(m)(2).product, Int.MaxValue)
+
+      Property.forAllG(Gen.choose(1, x)){ n: Int =>
+        val result = Property.property1[Int]{ i =>
+          Property.prop(i < n)
+        }(g, implicitly).check(param0, new AtomicBoolean(), _ => ())
+
+        result match {
+          case CheckResult.Falsified(_, 0, ICons(arg, INil())) if (m < arg.shrinks) && (arg.shrinks < Integer.SIZE) && (arg.value == n) =>
+            true
+          case fail =>
+            sys.error(fail.toString)
+        }
+      }
+    }(implicitly, Shrink.empty)
+  }
 }
