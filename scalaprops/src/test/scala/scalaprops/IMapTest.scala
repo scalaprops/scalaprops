49a549ba2c174686d513297b9429bf1614d12afc
xuwei-k <6b656e6a69@gmail.com>
2015-06-07 19:08:30 +0900

    add more IMapTest

--- scalaprops/src/test/scala/scalaprops/IMapTest.scala
+++ scalaprops/src/test/scala/scalaprops/IMapTest.scala
@@ -13,4 +13,18 @@ object IMapTest extends Scalaprops {
       scalazlaws.traverse.all[({type l[a] = Int ==>> a})#l]
     )
 
+  val bifoldable = scalazlaws.bifoldable.all[==>>]
+  val order = scalazlaws.order.all[Int ==>> Int]
+  val monoid = scalazlaws.monoid.all[Int ==>> Int]
+
+  val conjunction = {
+    implicit def imapConjunctionGen[A: Gen: Order, B: Gen]: Gen[((A ==>> B) @@ Tags.Conjunction)] =
+      Tag.subst(Gen[A ==>> B])
+
+    implicit val s = IMap.mapIntersection[Int, Int] // poor type inference...
+    implicit val e = Tags.Conjunction.subst(Equal[Int ==>> Int])
+
+    scalazlaws.semigroup.all[(Int ==>> Int) @@ Tags.Conjunction]
+  }
+
 }
