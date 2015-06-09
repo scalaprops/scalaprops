8f183e36c83bee2ec9a8b46e4e5f015a2f8e9aa5
xuwei-k <6b656e6a69@gmail.com>
2015-06-10 01:37:50 +0900

    fix FunctionEqual#f1

--- scalaprops/src/test/scala/scalaprops/FunctionEqual.scala
+++ scalaprops/src/test/scala/scalaprops/FunctionEqual.scala
@@ -6,14 +6,13 @@ import scalaz._
 object FunctionEqual extends FunctionEqual(30)
 
 sealed class FunctionEqual(size: Int) {
-  implicit def f1[A1: Gen, B](implicit B: Equal[B]): Equal[A1 => B] =
-    Equal.equal( (x, y) =>
-      Foldable[IList].all(
-        Gen[IList[A1]].sample(size = size, seed = Random.nextLong())
-      )(a =>
-        B.equal(x(a), y(a))
-      )
+  implicit def f1[A1: Gen, B](implicit B: Equal[B]): Equal[A1 => B] = {
+    val values = Gen[IList[A1]].sample(size = size, seed = Random.nextLong())
+
+    Equal.equal((x, y) =>
+      Foldable[IList].all(values)(a => B.equal(x(a), y(a)))
     )
+  }
 
   implicit def f2[A1: Gen, A2: Gen, B](implicit B: Equal[B]): Equal[(A1, A2) => B] =
     f1[(A1, A2), B].contramap(_.tupled)
