fff887f78437167540a23e951adb9d2184a1d699
xuwei-k <6b656e6a69@gmail.com>
2015-06-20 01:50:42 +0900

    scalaz 7.1.3

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object build extends Build {
   private[this] val scalazlawsName = "scalaprops-scalazlaws"
   private[this] val scalapropsName = "scalaprops"
 
-  private[this] val scalazVersion = "7.1.2"
+  private[this] val scalazVersion = "7.1.3"
 
   val modules: List[String] = (
     coreName ::
