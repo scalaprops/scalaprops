86053ef78a3ffe98a58d9b52c48c787c9877bb36
xuwei-k <6b656e6a69@gmail.com>
2015-06-29 04:33:07 +0900

    add CaseClassExample

---
+++ scalaprops/src/test/scala/scalaprops/CaseClassExample.scala
@@ -0,0 +1,23 @@
+package scalaprops
+
+object CaseClassExample {
+
+  final case class UserId(value: Long)
+  object UserId{
+    implicit val gen: Gen[UserId] = Gen.from(apply _)
+    implicit val cogen: Cogen[UserId] = Cogen.from(unapply _)
+  }
+
+  Gen[UserId]
+  Cogen[UserId]
+
+  final case class CaseClass2(a: Long, b: Boolean)
+  object CaseClass2{
+    implicit val gen: Gen[CaseClass2] = Gen.from(apply _)
+    implicit val cogen: Cogen[CaseClass2] = Cogen.from2(unapply _)
+  }
+
+  Gen[CaseClass2]
+  Cogen[CaseClass2]
+
+}
