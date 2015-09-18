f3f36ae63a585e037cc4a290ad38f445f8f23c4a
xuwei-k <6b656e6a69@gmail.com>
2015-09-19 08:37:38 +0900

    Divide and Divisible laws

---
+++ scalazlaws/src/main/scala/scalaprops/scalazlaws/divide.scala
@@ -0,0 +1,20 @@
+package scalaprops
+package scalazlaws
+
+import scalaprops.Property.forAll
+import scalaz._
+
+object divide {
+  def composition[F[_], A](implicit F: Divide[F], G: Gen[F[A]], E: Equal[F[A]]): Property =
+    forAll(F.divideLaw.composition[A] _)
+
+  def laws[F[_]: Divide](implicit
+    G: Gen[F[Int]], E: Equal[F[Int]]
+  ): Properties[ScalazLaw] =
+    Properties.properties(ScalazLaw.divide)(
+      ScalazLaw.divideComposition -> this.composition[F, Int]
+    )
+
+  def all[F[_]: Divide](implicit G: Gen[F[Int]], E: Equal[F[Int]]): Properties[ScalazLaw] =
+    Properties.fromProps(ScalazLaw.divideAll, divide.laws[F], contravariant.all[F])
+}
