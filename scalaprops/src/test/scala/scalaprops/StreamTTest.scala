289ffcabe87b2818f1c87d76051d1ec77fab7fb3
xuwei-k <6b656e6a69@gmail.com>
2015-09-22 09:12:07 +0900

    ({type l[a] = StreamT[F, a]})#l <~> ({type l[a] = F[Stream[a]]})#l

--- scalaprops/src/test/scala/scalaprops/StreamTTest.scala
+++ scalaprops/src/test/scala/scalaprops/StreamTTest.scala
@@ -3,39 +3,56 @@ package scalaprops
 import scalaz._
 import scalaz.std.anyVal._
 import scalaz.std.stream._
+import scalaz.Isomorphism._
 
 object StreamTTest extends Scalaprops {
 
+  private[this] def iso[F[_]: Monad]: ({type l[a] = StreamT[F, a]})#l <~> ({type l[a] = F[Stream[a]]})#l =
+    new IsoFunctorTemplate[({type l[a] = StreamT[F, a]})#l, ({type l[a] = F[Stream[a]]})#l] {
+      override def to[A](fa: StreamT[F, A]) =
+        fa.toStream
+
+      override def from[A](ga: F[Stream[A]]) =
+        StreamT.fromStream(ga)
+    }
+
   val testId = {
     import scalaz.Id._
-    type F[A] = StreamT[Id, A]
+    type G[A] = Id[A]
+    type F[A] = StreamT[G, A]
     Properties.list(
       scalazlaws.monadPlus.all[F],
+      scalazlaws.iso.all(iso[G].unlift[Byte]),
       scalazlaws.equal.all[F[Int]]
     )
   }
 
   val testOneOrTwo = {
-    type OneOrTwo[A] = OneAnd[Maybe, A]
-    type F[A] = StreamT[OneOrTwo, A]
+    type G[A] = OneAnd[Maybe, A]
+    type F[A] = StreamT[G, A]
     Properties.list(
       scalazlaws.monadPlus.all[F],
+      scalazlaws.iso.all(iso[G].unlift[Byte]),
       scalazlaws.equal.all[F[Int]]
     )
   }.andThenParam(Param.maxSize(2))
 
   val testMaybe = {
-    type F[A] = StreamT[Maybe, A]
+    type G[A] = Maybe[A]
+    type F[A] = StreamT[G, A]
     Properties.list(
       scalazlaws.monadPlus.all[F],
+      scalazlaws.iso.all(iso[G].unlift[Byte]),
       scalazlaws.equal.all[F[Int]]
     )
   }
 
   val iList = {
-    type F[A] = StreamT[IList, A]
+    type G[A] = IList[A]
+    type F[A] = StreamT[G, A]
     Properties.list(
       scalazlaws.monadPlus.all[F],
+      scalazlaws.iso.all(iso[G].unlift[Byte]),
       scalazlaws.equal.all[F[Int]]
     )
   }.andThenParam(Param.maxSize(3))
