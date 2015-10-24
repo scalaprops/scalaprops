491843e53c55b7cfb2ae5ffddba93fe2f547e98e
xuwei-k <6b656e6a69@gmail.com>
2015-10-25 01:45:51 +0900

    avoid ExecutionContext.Implicits.global

--- scalaprops/src/test/scala/scalaprops/std/FutureTest.scala
+++ scalaprops/src/test/scala/scalaprops/std/FutureTest.scala
@@ -2,16 +2,20 @@ package scalaprops
 package std
 
 import _root_.java.util.concurrent.TimeUnit
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import scalaz._
+import scala.concurrent.{Await, Future, ExecutionContext}
+import scalaz.Equal
 import scalaz.std.anyVal._
 import scalaz.std.scalaFuture._
 
 object FutureTest extends Scalaprops {
   private[this] implicit def genFuture[A](implicit A: Gen[A]) =
     A.map(Future.successful)
+
+  private[this] implicit val singleThreadExecutionContext = new ExecutionContext {
+    def execute(runnable: Runnable): Unit = runnable.run
+    def reportFailure(cause: Throwable): Unit = cause.printStackTrace
+  }
 
   private[this] implicit def equalFuture[A: Equal]: Equal[Future[A]] =
     Equal.equalBy(Await.result(_, Duration(5, TimeUnit.SECONDS)))
