3707556dece5ae21ebb31301501b3ae7ac382210
xuwei-k <6b656e6a69@gmail.com>
2015-11-09 07:51:49 +0900

    add Tuple2Test

---
+++ scalaprops/src/test/scala/scalaprops/Tuple2Test.scala
@@ -0,0 +1,26 @@
+package scalaprops
+
+import scalaz._
+import scalaz.std.tuple._
+import scalaz.std.anyVal._
+
+object Tuple2Test extends Scalaprops {
+
+  val bitraverse = scalazlaws.bitraverse.all[Tuple2]
+  val associative = scalazlaws.associative.all[Tuple2]
+
+  val laws1 = {
+    type F[A] = (IList[Boolean], A)
+    Properties.list(
+      scalazlaws.monad.all[F],
+      scalazlaws.comonad.all[F],
+      scalazlaws.traverse.all[F]
+    )
+  }
+
+  val laws0 = Properties.list(
+    scalazlaws.monoid.all[(IList[Boolean], IList[Boolean])],
+    scalazlaws.order.all[(Byte, Byte)]
+  )
+
+}
