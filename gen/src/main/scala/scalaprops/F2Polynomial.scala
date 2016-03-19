38749b7c37ebad81b73faef690584fdd1c17a846
xuwei-k <6b656e6a69@gmail.com>
2016-03-19 21:21:00 +0900

    TinyMT32
    (cherry picked from commit f984293a9484ef2605228cb816fd9d04e6818b1e)

---
+++ gen/src/main/scala/scalaprops/F2Polynomial.scala
@@ -0,0 +1,243 @@
+package scalaprops
+
+import java.math.BigInteger
+import scala.annotation.tailrec
+
+/** Polynomial over the field of two elements. <b>F</b><sub>2</sub>[t]
+  *
+  * This class is immutable.
+  *
+  * Caution: This class is not efficient for large polynomial.
+  *
+  * @author M. Saito
+  */
+object F2Polynomial {
+  /** Polynomial X<sup>1</sup> + 0. */
+  private[scalaprops] val X = new F2Polynomial("10")
+
+  /**
+    * If zero, this method returns -1, otherwise, returns the degree of
+    * polynomial.
+    *
+    * @param pol
+    * polynomial
+    * @return degree of polynomial pol
+    */
+  private def degree(pol: BigInteger): Int = {
+    if (pol == BigInteger.ZERO) {
+      -1
+    } else {
+      pol.bitLength - 1
+    }
+  }
+
+  /** Multiplication of BigIntegers which represents coefficient of polynomials.
+    *
+    * @param x
+    * polynomial
+    * @param y
+    * polynomial
+    * @return the result of multiplication
+    */
+  private def mul(x: BigInteger, y: BigInteger): BigInteger = {
+    @tailrec def loop(z: BigInteger, v: BigInteger, w: BigInteger): BigInteger = {
+      if(w != BigInteger.ZERO) {
+        val z0 = if (w.and(BigInteger.ONE).testBit(0)) {
+          z.xor(v)
+        } else z
+        loop(z0, v.shiftLeft(1), w.shiftRight(1))
+      } else z
+    }
+    loop(BigInteger.ZERO, x, y)
+  }
+
+  /**
+    * Calculate residue of polynomial y divided by that polynomial. Using means
+    * return y % that if y and that were int.
+    *
+    * @param y dividee
+    * @param that divider
+    * @return residue
+    */
+  private def mod(y: BigInteger, that: BigInteger): BigInteger = {
+    val deg = degree(that)
+    val diff = degree(y) - deg
+    if (diff < 0) {
+      y
+    } else if (diff == 0) {
+      y.xor(that)
+    } else {
+      var z: BigInteger = y
+      var x: BigInteger = that.shiftLeft(diff)
+      z = z.xor(x)
+      var zdeg: Int = z.bitLength - 1
+      while (zdeg >= deg) {
+        x = x.shiftRight(x.bitLength - 1 - zdeg)
+        z = z.xor(x)
+        zdeg = z.bitLength - 1
+      }
+      z
+    }
+  }
+
+  /** returns x<sup>pow</sup> % mod.
+    *
+    * @param x polynomial
+    * @param power exponent
+    * @param mod polynomial
+    * @return polynomial whose degree is less than mod polynomial
+    */
+  private def powerMod(x: BigInteger, power: BigInteger, mod: BigInteger): BigInteger = {
+    var z: BigInteger = BigInteger.ONE
+    var s: BigInteger = x
+    var pow: BigInteger = power
+    while (!(pow == BigInteger.ZERO)) {
+      if (pow.and(BigInteger.ONE).testBit(0)) {
+        z = mul(z, s)
+        z = this.mod(z, mod)
+      }
+      s = mul(s, s)
+      s = this.mod(s, mod)
+      pow = pow.shiftRight(1)
+    }
+    z
+  }
+}
+
+/** @param pol internal representation of polynomial. */
+final class F2Polynomial private(private val pol: BigInteger) {
+
+  /** constructor from string with radix.
+    *
+    * @param value
+    * a string consists of numbers of radix
+    * @param radix
+    * radix of the number of val
+    */
+  def this(value: String, radix: Int) {
+    this(new BigInteger(value, radix))
+  }
+
+  /** constructor from string of 0 and 1.
+    *
+    * @param value
+    * a string consists of 0 and 1
+    */
+  def this(value: String) {
+    this(new BigInteger(value, 2))
+  }
+
+  /** If zero, this method returns -1, otherwise, returns the degree of polynomial.
+    *
+    * @return degree of this polynomial
+    */
+  def degree: Int =
+    F2Polynomial.degree(pol)
+
+  /** Addition over <b>F<sub>2</sub></b>[t].
+    *
+    * @param that Polynomial
+    * @return result of addition
+    */
+  def add(that: F2Polynomial): F2Polynomial =
+    new F2Polynomial(this.pol.xor(that.pol))
+
+  /** Multiplication over <b>F<sub>2</sub></b>[t].
+    *
+    * @param that Polynomial
+    * @return result of multiplication
+    */
+  def mul(that: F2Polynomial): F2Polynomial =
+    if (this.degree >= that.degree) {
+      new F2Polynomial(F2Polynomial.mul(this.pol, that.pol))
+    } else {
+      new F2Polynomial(F2Polynomial.mul(that.pol, this.pol))
+    }
+
+  /** return coefficient of specified term, returned value is zero or one.
+    *
+    * @param index degree of term
+    * @return coefficient of specified term
+    */
+  def getCoefficient(index: Int): Int =
+    if (pol.testBit(index)) {
+      1
+    } else {
+      0
+    }
+
+  /**
+    * Calculate residue of this polynomial divided by that polynomial. Using
+    * means return this % that if this and that were int.
+    *
+    * @param that divider
+    * @return residue
+    */
+  def mod(that: F2Polynomial): F2Polynomial =
+    new F2Polynomial(F2Polynomial.mod(this.pol, that.pol))
+
+  /** power of this polynomial, this<sup>pow</sup>.
+    *
+    * @param pow exponent
+    * @return this<sup>pow</sup>
+    */
+  def power(pow: BigInteger): F2Polynomial =
+    new F2Polynomial(power(this.pol, pow))
+
+  /** power of polynomial x, x<sup>pow</sup>.
+    *
+    * @param x polynomial
+    * @param power exponent
+    * @return the result of power
+    */
+  private def power(x: BigInteger, power: BigInteger): BigInteger = {
+    var z: BigInteger = BigInteger.ONE
+    var v: BigInteger = x
+    var pow: BigInteger = power
+    while (!(pow == BigInteger.ZERO)) {
+      if (pow.and(BigInteger.ONE).testBit(0)) {
+        z = F2Polynomial.mul(z, v)
+      }
+      v = F2Polynomial.mul(v, v)
+      pow = pow.shiftRight(1)
+    }
+    z
+  }
+
+  /** returns this<sup>pow</sup> % mod.
+    *
+    * @param pow exponent
+    * @param mod polynomial
+    * @return polynomial whose degree is less than mod polynomial
+    */
+  def powerMod(pow: BigInteger, mod: F2Polynomial): F2Polynomial = {
+    new F2Polynomial(F2Polynomial.powerMod(this.pol, pow, mod.pol))
+  }
+
+  /** return binary format representation of polynomial.
+    *
+    * @return binary format string
+    */
+  override def toString: String =
+    toString(2)
+
+  /** return base format representation of polynomial.
+    *
+    * @param base base of format
+    * @return base format string
+    */
+  def toString(base: Int): String =
+    pol.toString(base)
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case that: F2Polynomial =>
+        this.pol == that.pol
+      case _ =>
+        false
+    }
+  }
+
+  override def hashCode: Int =
+    pol.hashCode
+}
---
+++ scalaprops/src/test/scala/scalaprops/TinyMT32ParameterTest.scala
@@ -0,0 +1,41 @@
+package scalaprops
+
+object TinyMT32ParameterTest extends Scalaprops{
+
+  val testGetThreadLocalParameter = Property.forAll{
+    val p0 = TinyMT32Parameter.getThreadLocalParameter(0)
+    val p1 = TinyMT32Parameter.getThreadLocalParameter(1)
+    val pm2 = TinyMT32Parameter.getThreadLocalParameter(-2)
+    assert(p0.mat1 != p1.mat1)
+    p0.mat1 != pm2.mat1
+  }
+
+  val testGetDefaultParameter = Property.forAll{
+    val p = TinyMT32Parameter.getDefaultParameter
+    //d8524022ed8dff4a8dcc50c798faba43,32,0,8f7011ee,fc78ff1f,3793fdff,63,0
+    val pol = new F2Polynomial("d8524022ed8dff4a8dcc50c798faba43", 16)
+    val id = 0
+    val mat1 = 0x8f7011ee
+    val mat2 = 0xfc78ff1f
+    val tmat = 0x3793fdff
+    val weight = 63
+    val delta = 0
+    assert(pol == p.characteristic)
+    assert(id == p.id)
+    assert(mat1 == p.mat1)
+    assert(mat1 == p.getMat1(1))
+    assert(0 == p.getMat1(2))
+    assert(mat2 == p.mat2)
+    assert(mat2 == p.getMat2(3))
+    assert(0 == p.getMat2(4))
+    assert(tmat == p.tmat)
+    assert(tmat == p.getTmat(5))
+    assert(0 == p.getTmat(6))
+    assert((0x3f800000 | (tmat >>> 9)) == p.getTmatFloat(7))
+    assert(0x3f800000 == p.getTmatFloat(8))
+    assert(weight == p.weight)
+    assert(delta == p.delta)
+    true
+  }
+
+}
