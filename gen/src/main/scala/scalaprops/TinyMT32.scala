f984293a9484ef2605228cb816fd9d04e6818b1e
xuwei-k <6b656e6a69@gmail.com>
2016-03-19 21:21:00 +0900

    TinyMT32

---
+++ gen/src/main/scala/scalaprops/TinyMT32.scala
@@ -0,0 +1,559 @@
+package scalaprops
+
+import java.math.BigInteger
+
+import scala.annotation.tailrec
+
+/** TinyMT is a pseudo random number generator.
+  *
+  * <p>
+  * To get an instance, call `TinyMT32.getDefault`
+  * </p>
+  * <p>
+  * This class supports jump function. User can get an array of pseudo random
+  * number generators by calling `TinyMT32#getDefaultArray`
+  * </p>
+  *
+  * @author M. Saito
+  * @see [[http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/TINYMT/index.html TinyMT web page]]
+  */
+object TinyMT32 {
+  private final case class MutableState(
+    var st0: Int,
+    var st1: Int,
+    var st2: Int,
+    var st3: Int,
+    var param: TinyMT32Parameter
+  ) {
+    def asImmutable: TinyMT32 =
+      new TinyMT32(
+        st0 = st0,
+        st1 = st1,
+        st2 = st2,
+        st3 = st3,
+        parameter = param
+      )
+
+    def next(): this.type = {
+      var x = 0
+      var y = 0
+      y = st3
+      x = (st0 & TinyMT32.MASK) ^ st1 ^ st2
+      x ^= (x << TinyMT32.SH0)
+      y ^= (y >>> TinyMT32.SH0) ^ x
+      st0 = st1
+      st3 = y
+      st1 = st2 ^ param.getMat1(y)
+      st2 = (x ^ (y << TinyMT32.SH1)) ^ param.getMat2(y)
+      this
+    }
+  }
+
+  /** bit size of int. */
+  private val INT_SIZE: Int = 32
+  /** least long over int. */
+  private val LONG_LIMIT: Long = 0x100000000L
+  /** initialize shift. */
+  private val INITIALIZE_SHIFT: Int = 27
+  /** initialize shift. */
+  private val INITIALIZE_SHIFT2: Int = 30
+  /** magic number. */
+  private val MAGIC_NUMBER1: Int = 1664525
+  /** magic number. */
+  private val MAGIC_NUMBER2: Int = 1566083941
+  /** magic number. */
+  private val MAGIC_NUMBER3: Int = 1812433253
+  /** int to float shift. */
+  private val INT_TO_FLOAT_SHIFT: Int = 9
+  /** long to double shift. */
+  private val LONG_TO_DOUBLE_SHIFT: Int = 12
+  /** hexadecimal base. */
+  private val HEXA_DECIMAL_BASE: Int = 16
+  /** int to unsigned long mask. */
+  private val INT_TO_LONG_MASK: Long = 0xffffffffL
+  /** long to double mask. */
+  private val LONG_TO_DOUBLE_MASK: Long = 0x3ff0000000000000L
+  /** basic jump step.
+    * every jump step is a multiple of this step.
+    */
+  private val BASIC_JUMP_STEP: BigInteger = new BigInteger("2").pow(64)
+  /** mask pattern to limit internal size. */
+  private val MASK: Int = 0x7fffffff
+  /** fixed shift 0. */
+  private val SH0: Int = 1
+  /** fixed shift 1. */
+  private val SH1: Int = 10
+  /** fixed 8 bit shift. */
+  private val SH8: Int = 8
+  /** pre loop before generation. */
+  private val MIN_LOOP: Int = 8
+
+  /**
+    * Factory method which returns the TinyMT with the first generated
+    * parameter of TinyMTDC.
+    *
+    * @param seed
+    * seed of pseudo random numbers.
+    * @return TinyMT with the first parameter.
+    */
+  def getDefault(seed: String): TinyMT32 = {
+    val defaultParameter = TinyMT32Parameter.getDefaultParameter
+    (new TinyMT32(defaultParameter)).setSeed(seed)
+  }
+
+  /**
+    * Factory method which returns the TinyMT with the first generated
+    * parameter of TinyMTDC.
+    *
+    * @param seed
+    * seed of pseudo random numbers.
+    * @return TinyMT with the first parameter.
+    */
+  def getDefault(seed: Long): TinyMT32 = {
+    val defaultParameter = TinyMT32Parameter.getDefaultParameter
+    (new TinyMT32(defaultParameter)).setLongSeed(seed)
+  }
+
+  /** get default TinyMT32 with seeding by array.
+    *
+    * @param seeds seeds for initialization.
+    * @return random number generator TinyMT32
+    */
+  def getDefault(seeds: Array[Int]): TinyMT32 = {
+    val defaultParameter = TinyMT32Parameter.getDefaultParameter
+    new TinyMT32(defaultParameter).setSeed(seeds)
+  }
+
+  /**
+    * Factory method which returns the TinyMT with the first generated
+    * parameter of TinyMTDC. `System#nanoTime` and
+    * `Thread#getId()` are used for seed.
+    *
+    * @return TinyMT with the first parameter.
+    */
+  def getDefault(): TinyMT32 = {
+    val seed = new Array[Int](4)
+    val time: Long = System.nanoTime
+    val threadId: Long = Thread.currentThread.getId
+    seed(0) = (time >>> INT_SIZE).toInt
+    seed(1) = time.toInt
+    seed(2) = (threadId >>> INT_SIZE).toInt
+    seed(3) = threadId.toInt
+    val defaultParameter = TinyMT32Parameter.getDefaultParameter
+    new TinyMT32(defaultParameter).setSeed(seed)
+  }
+
+  /**
+    * make and return an array of TinyMT. Each element has the same
+    * characteristic polynomial with TinyMT gotten by getDefaultMT. Especially,
+    * the first element is just same as default TinyMT. The second element has
+    * the state of <b>jump</b> * 2<sup>64</sup> steps after the first element.
+    * In other word, the first element will generate the same sequence with the
+    * second element, after <b>jump</b> * 2<sup>64</sup> pseudo random number
+    * generation.
+    *
+    * @param count
+    * number of TinyMT to be created.
+    * @param seed
+    * seed of first element
+    * @param jump
+    * step is jump * 2<sup>64</sup>
+    * @return array of TinyMT
+    */
+  def getDefaultArray(count: Int, seed: Long, jump: Long): Array[TinyMT32] = {
+    val tiny: TinyMT32 = getDefault(seed)
+    tiny.getJumpedArray(count, jump)
+  }
+
+  /**
+    * Make and return an array of TinyMT. Each element has the same
+    * characteristic polynomial with TinyMT gotten by getDefaultMT. Especially,
+    * the first element is just same as default TinyMT. The second element has
+    * the state of <b>jump</b> * 2<sup>64</sup> steps after the first element.
+    * In other word, the first element will generate the same sequence with the
+    * second element, after <b>jump</b> * 2<sup>64</sup> pseudo random number
+    * generation.
+    *
+    * This is equals to TinyMT32.getDefault(seed).getJumpedArray(count, jump);
+    *
+    * @param count
+    * number of TinyMT to be created.
+    * @param seed
+    * seed of first element
+    * @param jump
+    * step is jump * 2<sup>64</sup>
+    * @return array of TinyMT
+    */
+  def getDefaultArray(count: Int, seed: String, jump: Long): Array[TinyMT32] = {
+    val tiny = getDefault(seed)
+    tiny.getJumpedArray(count, jump)
+  }
+
+  /** return TinyMT32 instance whose parameter has ID = 1.
+    *
+    * @param threadId thread ID
+    * @return TinyMT32 instance
+    */
+  def getThreadLocal(threadId: Long): TinyMT32 = {
+    new TinyMT32(TinyMT32Parameter.getThreadLocalParameter(threadId))
+  }
+}
+
+/**
+  * @param parameter parameters for this generator.
+  */
+final case class TinyMT32(
+  private val st0: Int,
+  private val st1: Int,
+  private val st2: Int,
+  private val st3: Int,
+  private val parameter: TinyMT32Parameter
+) extends Rand {
+
+  private def asMutable: TinyMT32.MutableState =
+    TinyMT32.MutableState(
+      st0 = st0,
+      st1 = st1,
+      st2 = st2,
+      st3 = st3,
+      param = parameter
+    )
+
+  /** Constructor from a parameter.
+    *
+    * @param param
+    * a parameter generated by TinyMTDC
+    */
+  private def this(param: TinyMT32Parameter) {
+    this(
+      st0 = 0,
+      st1 = 0,
+      st2 = 0,
+      st3 = 0,
+      parameter = param
+    )
+  }
+
+  def nextInt: (TinyMT32, Int) = {
+    val s = nextState
+    (s, s.output)
+  }
+
+  override def nextLong: (TinyMT32, Long) = {
+    val (s0, x0) = nextInt
+    val t: Long = (x0: Long) << TinyMT32.INT_SIZE
+    val (s1, x1) = s0.nextInt
+    val x2 = t | (x1 & TinyMT32.INT_TO_LONG_MASK)
+    (s1, x2)
+  }
+
+  def reseed(seed: Long): TinyMT32 =
+    setLongSeed(seed)
+
+  def setLongSeed(seed: Long): TinyMT32 = {
+    if ((seed >= 0) && (seed < TinyMT32.LONG_LIMIT)) {
+      setIntSeed(seed.toInt)
+    }
+    else {
+      val tmp = new Array[Int](2)
+      tmp(0) = (seed & 0xffffffff).toInt
+      tmp(1) = (seed >>> TinyMT32.INT_SIZE).toInt
+      setSeed(tmp)
+    }
+  }
+
+  def setSeed(seed: String): TinyMT32 = {
+    val intSeeds = new Array[Int](seed.length)
+    @tailrec def loop(i: Int): Unit = {
+      if (i < intSeeds.length) {
+        intSeeds(i) = seed.charAt(i)
+        loop(i + 1)
+      }
+    }
+    loop(0)
+    setSeed(intSeeds)
+  }
+
+  def setSeed(seeds: Array[Int]): TinyMT32 = {
+    val lag: Int = 1
+    val mid: Int = 1
+    val size: Int = 4
+    var i: Int = 0
+    var j: Int = 0
+    var count: Int = 0
+    var r: Int = 0
+    val keyLength: Int = seeds.length
+    val status = Array[Int](0, parameter.mat1, parameter.mat2, parameter.tmat)
+    if (keyLength + 1 > TinyMT32.MIN_LOOP) {
+      count = keyLength + 1
+    }
+    else {
+      count = TinyMT32.MIN_LOOP
+    }
+    r = iniFunc1(status(0) ^ status(mid % size) ^ status((size - 1) % size))
+    status(mid % size) += r
+    r += keyLength
+    status((mid + lag) % size) += r
+    status(0) = r
+    count -= 1
+
+    {
+      i = 1
+      j = 0
+      while ((j < count) && (j < keyLength)) {
+        r = iniFunc1(status(i % size) ^ status((i + mid) % size) ^ status((i + size - 1) % size))
+        status((i + mid) % size) += r
+        r += seeds(j) + i
+        status((i + mid + lag) % size) += r
+        status(i % size) = r
+        i = (i + 1) % size
+        j += 1
+      }
+    }
+
+    while (j < count) {
+      r = iniFunc1(status(i % size) ^ status((i + mid) % size) ^ status((i + size - 1) % size))
+      status((i + mid) % size) += r
+      r += i
+      status((i + mid + lag) % size) += r
+      status(i % size) = r
+      i = (i + 1) % size
+      j += 1
+    }
+
+    {
+      j = 0
+      while (j < size) {
+        r = iniFunc2(status(i % size) + status((i + mid) % size) + status((i + size - 1) % size))
+        status((i + mid) % size) ^= r
+        r -= i
+        status((i + mid + lag) % size) ^= r
+        status(i % size) = r
+        i = (i + 1) % size
+        j += 1
+      }
+    }
+
+    val x = new TinyMT32(
+      st0 = status(0),
+      st1 = status(1),
+      st2 = status(2),
+      st3 = status(3),
+      parameter = this.parameter
+    ).periodCertification0
+
+    @tailrec
+    def loop(z: TinyMT32.MutableState, i: Int): TinyMT32 =
+      if(i < TinyMT32.MIN_LOOP) {
+        loop(z.next(), i + 1)
+      } else {
+        z.asImmutable
+      }
+
+    loop(x.asMutable, 0)
+  }
+
+  /** sub function of initialization.
+    *
+    * @param x input number
+    * @return scrambled integer
+    */
+  private def iniFunc1(x: Int): Int =
+    (x ^ (x >>> TinyMT32.INITIALIZE_SHIFT)) * TinyMT32.MAGIC_NUMBER1
+
+  /** sub function of initialization.
+    *
+    * @param x input number
+    * @return scrambled integer
+    */
+  private def iniFunc2(x: Int): Int =
+    (x ^ (x >>> TinyMT32.INITIALIZE_SHIFT)) * TinyMT32.MAGIC_NUMBER2
+
+  def setIntSeed(seed: Int): TinyMT32 = {
+    val counterMask = 3
+    val status = new Array[Int](4)
+    status(0) = seed
+    status(1) = parameter.mat1
+    status(2) = parameter.mat2
+    status(3) = parameter.tmat
+
+    {
+      @tailrec
+      def loop0(i: Int): Unit = {
+        if(i < TinyMT32.MIN_LOOP) {
+          status(i & counterMask) ^= i + TinyMT32.MAGIC_NUMBER3 * (status((i - 1) & counterMask) ^ (status((i - 1) & counterMask) >>> TinyMT32.INITIALIZE_SHIFT2))
+          loop0(i + 1)
+        }
+      }
+      loop0(1)
+    }
+
+    val x = new TinyMT32(
+      st0 = status(0),
+      st1 = status(1),
+      st2 = status(2),
+      st3 = status(3),
+      parameter = this.parameter
+    ).periodCertification0
+
+    @tailrec
+    def loop(i: Int, z: TinyMT32.MutableState): TinyMT32 =
+      if(i < TinyMT32.MIN_LOOP) {
+        loop(i + 1, z.next())
+      } else {
+        z.asImmutable
+      }
+
+    loop(0, x.asMutable)
+  }
+
+  private def periodCertification0: TinyMT32 = {
+    if (((st0 & TinyMT32.MASK) == 0) && (st1 == 0) && (st2 == 0) && (st3 == 0)) {
+      new TinyMT32(
+        st0 = 'T',
+        st1 = 'I',
+        st2 = 'N',
+        st3 = 'Y',
+        parameter = this.parameter
+      )
+    } else this
+  }
+
+  /** The state transition function. This function is F<sub>2</sub>-linear. */
+  private def nextState: TinyMT32 = {
+    var x = 0
+    var y = 0
+    y = st3
+    x = (st0 & TinyMT32.MASK) ^ st1 ^ st2
+    x ^= (x << TinyMT32.SH0)
+    y ^= (y >>> TinyMT32.SH0) ^ x
+    val x0 = st1
+    val x3 = y
+    val x1 = st2 ^ parameter.getMat1(y)
+    val x2 = (x ^ (y << TinyMT32.SH1)) ^ parameter.getMat2(y)
+    new TinyMT32(
+      st0 = x0,
+      st1 = x1,
+      st2 = x2,
+      st3 = x3,
+      parameter = this.parameter
+    )
+  }
+
+  /** The output function.
+    *
+    * @return pseudo random number
+    */
+  private def output: Int = {
+    var t0: Int = 0
+    var t1: Int = 0
+    t0 = st3
+    t1 = st0 + (st2 >>> TinyMT32.SH8)
+    t0 ^= t1
+    t0 ^= parameter.getTmat(t1)
+    t0
+  }
+
+  /** make float random.
+    *
+    * @return float output.
+    */
+  private def outputFloat: Float = {
+    var t0: Int = 0
+    var t1: Int = 0
+    t0 = st3
+    t1 = st0 + (st2 >>> TinyMT32.SH8)
+    t0 ^= t1
+    t0 = (t0 >>> TinyMT32.INT_TO_FLOAT_SHIFT) ^ parameter.getTmatFloat(t1)
+    java.lang.Float.intBitsToFloat(t0) - 1.0f
+  }
+
+  private def add0(that: TinyMT32): TinyMT32 =
+    new TinyMT32(
+      st0 = this.st0 ^ that.st0,
+      st1 = this.st1 ^ that.st1,
+      st2 = this.st2 ^ that.st2,
+      st3 = this.st3 ^ that.st3,
+      parameter = this.parameter
+    )
+
+
+  /** jump function.
+    *
+    * @param pol jump polynomial
+    * @return jumped new TinyMT
+    */
+  private def jump(pol: F2Polynomial): TinyMT32 = {
+    val degree = pol.degree
+
+    // TODO avoid allocate new instance each loop
+    @tailrec
+    def loop(i: Int, src: TinyMT32, that: TinyMT32): TinyMT32 =
+      if (i > degree) {
+        that
+      } else if (pol.getCoefficient(i) == 1) {
+        loop(i + 1, src.nextState, that.add0(src))
+      } else {
+        loop(i + 1, src.nextState, that)
+      }
+
+    loop(0, this, new TinyMT32(this.parameter))
+  }
+
+  /**
+    * Make and return an array of TinyMT. Each element of the array has the
+    * same characteristic polynomial with this. Especially, the first element
+    * is just same as this. The second element has the state of <b>jump</b> *
+    * 2<sup>64</sup> steps after the first element. In other word, the first
+    * element will generate the same sequence with the second element, after
+    * <b>jump</b> * 2<sup>64</sup> pseudo random number generation.
+    *
+    * <p>
+    * Note: Do not call any setSeed methods after jump. Seeding will cancel
+    * the effect of jump.
+    * </p>
+    *
+    * @param count number of arrays
+    * @param jump  jump step
+    * @return jumped array of TinyMT32.
+    */
+  def getJumpedArray(count: Int, jump: Long): Array[TinyMT32] = {
+    val tiny = new Array[TinyMT32](count)
+    tiny(0) = this
+    val poly = tiny(0).parameter.characteristic
+    val pow = TinyMT32.BASIC_JUMP_STEP.multiply(BigInteger.valueOf(jump))
+    val jumpPoly = F2Polynomial.X.powerMod(pow, poly)
+    @tailrec def loop(i: Int): Unit = {
+      if (i < count) {
+        tiny(i) = tiny(i - 1).jump(jumpPoly)
+        loop(i + 1)
+      }
+    }
+    loop(1)
+    tiny
+  }
+
+  override def nextDouble: (TinyMT32, Double) = {
+    val temp = nextLong
+    val x: Long = (temp._2 >>> TinyMT32.LONG_TO_DOUBLE_SHIFT) | TinyMT32.LONG_TO_DOUBLE_MASK
+    (temp._1, java.lang.Double.longBitsToDouble(x) - 1.0)
+  }
+
+  def nextFloat: (TinyMT32, Float) = {
+    val s = nextState
+    (s, s.outputFloat)
+  }
+
+  /** return characteristic polynomial in hexadecimal format. */
+  def getCharacteristic: String =
+    parameter.characteristic.toString(TinyMT32.HEXA_DECIMAL_BASE)
+
+  /** return ID of TinyMT. ID is not unique in TinyMT. */
+  def getId: Int = parameter.id
+
+  /** return Delta of TinyMT. */
+  def getDelta: Int = parameter.delta
+
+  /** return Hamming weight of characteristic polynomial of TinyMT. */
+  def getWeight: Int = parameter.weight
+}
---
+++ scalaprops/src/test/scala/scalaprops/TinyMT32Test.scala
@@ -0,0 +1,192 @@
+package scalaprops
+
+import java.util.Arrays
+
+object TinyMT32Test extends Scalaprops {
+
+  val testGetDefault = Property.forAll{
+    var tiny = TinyMT32.getDefault(1)
+    val c_data = List(0x97b6d625, 0x3a86e2e1, 0xdd7305b1, 0x8e4ef1b0, 0xd60a5515, 0xe3b751f6, 0x7e073136, 0x82e5df8b, 0xa5e6b3a8, 0x2d91deed)
+    val output = (0 until c_data.length).map { i =>
+      val temp = tiny.nextInt
+      tiny = temp._1
+      temp._2
+    }
+    assert(output == c_data)
+    val weight: Int = 63
+    val delta: Int = 0
+    assert("d8524022ed8dff4a8dcc50c798faba43" == tiny.getCharacteristic)
+    assert(0 == tiny.getId)
+    assert(delta == tiny.getDelta)
+    weight == tiny.getWeight
+  }
+
+
+  val testGetDefaultString = Property.forAll{
+    var tiny = TinyMT32.getDefault("abcd")
+    val c_data = List(0x92264019, 0x3e048b87, 0x0618bd2a, 0x0b85252c, 0xf720f342, 0x73ac4297, 0x20974815, 0x8ac7a201, 0x7443b188, 0x0b7c6501)
+
+    val output = (0 until c_data.length).map { i =>
+      val temp = tiny.nextInt
+      tiny = temp._1
+      temp._2
+    }
+    output == c_data
+  }
+
+
+  val testGetDefaultSeedArray = Property.forAll{
+    val seeds = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    var tiny = TinyMT32.getDefault(seeds)
+    val c_data = List(0x7c14361a, 0x41b450a8, 0xfecc94ae, 0xd19693cd, 0x1d8b3ef7, 0xb5c88b3f, 0x45c79c71, 0xc8465811, 0x10fcf027, 0x4e3317e5)
+
+    val output = (0 until c_data.length).map { i =>
+      val temp = tiny.nextInt
+      tiny = temp._1
+      temp._2
+    }
+    output == c_data
+  }
+
+
+  val testGetDefaultArray = Property.forAll{
+    val tiny = TinyMT32.getDefaultArray(5, 1L, 1L)
+    val c_data = Vector[Vector[Int]](
+      Vector(0x97b6d625, 0x3a86e2e1, 0xdd7305b1, 0x8e4ef1b0, 0xd60a5515, 0xe3b751f6, 0x7e073136, 0x82e5df8b, 0xa5e6b3a8, 0x2d91deed),
+      Vector(0x069dd221, 0x139591eb, 0xb1bb01cb, 0xa59dfffc, 0x739f69a3, 0xa73eb87e, 0xe74ff5a4, 0x66add33a, 0x8058ebab, 0xd40b22ae),
+      Vector(0x04788045, 0x4ff9f6ac, 0xa2320522, 0x7b54307f, 0x54d3cee9, 0xa6dd6ea1, 0xceaa10dc, 0x0ed65e8d, 0xe1457e91, 0x154a1ab8),
+      Vector(0xd026d1ca, 0xebce67c6, 0xdc706eda, 0xbc8007cb, 0xe2eb9899, 0x399992f3, 0x0ab705e6, 0x8b79b94b, 0x8a5c9316, 0x16cc039f),
+      Vector(0xd15c9d16, 0xa767e149, 0xe9c73b4b, 0xaf69de00, 0x8319be3b, 0xecc79680, 0x8ab49653, 0x9bf1d861, 0x04c7dab6, 0xb0d5cf24)
+    )
+    c_data.zipWithIndex.foreach{ case (data, i) =>
+      val output = (0 until data.size).map{ _ =>
+        val temp = tiny(i).nextInt
+        tiny(i) = temp._1
+        temp._2
+      }
+      assert(output == data, s"$i $output $data")
+    }
+    true
+  }
+
+
+  val testGetDefaultArrayString = Property.forAll{
+    val tiny = TinyMT32.getDefaultArray(5, "tinyMT", 1)
+    val c_data = List(
+      List[Int](0xd0065635, 0x8d5e606e, 0x35292e8a, 0xf50272e0, 0x8fa1550b, 0x6aaaed21, 0xb0159b78, 0x4adbe52d, 0x1ef7779f, 0xfea8cab6),
+      List[Int](0x98a8d0c4, 0xf0d0d812, 0xd00df952, 0x5f40c35c, 0x11cc9cab, 0x8769ca59, 0xa06f3c6c, 0xe8285f96, 0x54005bb2, 0x483fec65),
+      List[Int](0x9835dd76, 0x82601445, 0xfd38ca2f, 0x4990fca2, 0x250a026c, 0x20045548, 0x3d23916c, 0x63eec834, 0x104f5606, 0x8dd4ee2f),
+      List[Int](0x343a6834, 0x55e090a9, 0xb00c8b76, 0xc8c3738a, 0xeabdb64b, 0x8ed5908f, 0x3f4393b6, 0xd57b28eb, 0x43725b82, 0x8e1c4a30),
+      List[Int](0x5c8e1f49, 0xbca358a9, 0x6189bbae, 0xe3e7eded, 0x6e2fa730, 0xe1a38ddc, 0x4bbb4104, 0x478baaa4, 0x521237a7, 0xbfcd41ad)
+    )
+
+    c_data.zipWithIndex.foreach{ case (data, i) =>
+      val output = (0 until data.size).map { _ =>
+        val temp = tiny(i).nextInt
+        tiny(i) = temp._1
+        temp._2
+      }
+      assert(output == data, i)
+    }
+    true
+  }
+
+
+  val testSeedArray = Property.forAll{
+    var tiny = TinyMT32.getDefault(1)
+    val ar = Array[Int](1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val c_data = List(0x7c14361a, 0x41b450a8, 0xfecc94ae, 0xd19693cd, 0x1d8b3ef7, 0xb5c88b3f, 0x45c79c71, 0xc8465811, 0x10fcf027, 0x4e3317e5)
+    tiny = tiny.setSeed(ar)
+
+    val output = (0 until c_data.length).map{ _ =>
+      val temp = tiny.nextInt
+      tiny = temp._1
+      temp._2
+    }
+    output == c_data
+  }
+
+
+  val testSetSeedLong = Property.forAll{
+    var tiny: TinyMT32 = TinyMT32.getDefault(100)
+    val output = new Array[Int](10)
+    val c_data = Array[Int](0xeead8165, 0x8715ef2c, 0x23598315, 0x5facb14e, 0x73e28fdf, 0x9826df55, 0x8536bb9c, 0x0c73d703, 0x7b174b03, 0x7ddb6aaf)
+
+    var i = 0
+    while (i < output.length) {
+      val temp = tiny.nextInt
+      output(i) = temp._2
+      tiny = temp._1
+      i += 1
+    }
+
+    assert(Arrays.equals(output, c_data))
+    tiny = TinyMT32.getDefault(0x1000000010000000L)
+    val c_data2 = Array[Int](0xfd520eb5, 0x6cdab063, 0x2877a46e, 0x5ed3fb03, 0xc1bb07ea, 0xfb872239, 0xd2e2bade, 0xa41ea287, 0x263e62d3, 0x94a5d5d1)
+
+    i = 0
+    while (i < output.length) {
+      val temp = tiny.nextInt
+      output(i) = temp._2
+      tiny = temp._1
+      i += 1
+    }
+
+    Arrays.equals(output, c_data2)
+  }
+
+
+  val testSetSeedArray = Property.forAll{
+    var tiny = TinyMT32.getDefault().setSeed("abcd")
+    val output = new Array[Int](10)
+    val c_data = Array[Int](0x92264019, 0x3e048b87, 0x0618bd2a, 0x0b85252c, 0xf720f342, 0x73ac4297, 0x20974815, 0x8ac7a201, 0x7443b188, 0x0b7c6501)
+
+    var i: Int = 0
+    while (i < output.length) {
+      val temp = tiny.nextInt
+      output(i) = temp._2
+      tiny = temp._1
+      i += 1
+    }
+
+    Arrays.equals(output, c_data)
+  }
+
+  val testNextFloat = Property.forAll{
+    var tiny = TinyMT32.getDefault().setIntSeed(1)
+    val c_data = Array[Float](0.5926336050f, 0.2286207080f, 0.8650363684f, 0.5558921099f, 0.8360951543f, 0.8895159960f, 0.4922972322f, 0.5113201141f, 0.6480515003f, 0.1780070662f)
+
+    var i = 0
+    while (i < c_data.length) {
+      val temp = tiny.nextFloat
+      tiny = temp._1
+      assert(math.abs(temp._2 - c_data(i)) < 0.00001, i)
+      i += 1
+    }
+    true
+  }
+
+
+  val testNextDouble = Property.forAll{
+    var tiny = TinyMT32.getDefault().setIntSeed(1)
+    val c_data = Array[Double](0.5926336137, 0.8650363501, 0.8360951594, 0.4922972448, 0.6480514799, 0.1497518912, 0.2052537946, 0.8592087870, 0.6802024140, 0.9528347664)
+
+    var i = 0
+    while (i < c_data.length) {
+      val temp = tiny.nextDouble
+      tiny = temp._1
+      assert(math.abs(temp._2 - c_data(i)) < 0.00000001, i)
+      i += 1
+    }
+    true
+  }
+
+  val testgetThreadLocal = Property.forAll{
+    var tiny: TinyMT32 = TinyMT32.getThreadLocal(0)
+    assert("80227acb382d7b47f3714bd1223bedaf" == tiny.getCharacteristic)
+    tiny = TinyMT32.getThreadLocal(1)
+    assert("db46f27d546507bdf3445acd188fa8a3" == tiny.getCharacteristic)
+    tiny = TinyMT32.getThreadLocal(-1)
+    "db46f27d546507bdf3445acd188fa8a3" == tiny.getCharacteristic
+  }
+}
