a68264c95e75299df57794aa0ad20f3787ff8a11
xuwei-k <6b656e6a69@gmail.com>
2016-04-02 01:44:12 +0900

    scalaz 7.2.2

--- project/Build.scala
+++ project/Build.scala
@@ -41,7 +41,7 @@ object build extends Build {
     CrossProject(id, file(id), CustomCrossType).settings(
       commonSettings: _*
     ).settings(
-      scalazVersion := "7.2.1",
+      scalazVersion := "7.2.2",
       scalaJSStage in Test := FastOptStage,
       scalaJSUseRhino in Global := false,
       jsEnv := NodeJSEnv().value,
