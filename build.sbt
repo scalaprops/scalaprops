c05e78674013925b46202bb8ea6a2ad1f69be019
xuwei-k <6b656e6a69@gmail.com>
2016-07-19 17:17:18 +0900

    use build.sbt
    - https://github.com/sbt/sbt/commit/f2f12022a5672c3f57b8e5fce4227da455ab8024
    - https://github.com/sbt/sbt/blob/v0.13.12/main/src/main/scala/sbt/Build.scala#L11

---
+++ build.sbt
@@ -0,0 +1,56 @@
+import UnidocKeys._
+import build._
+
+lazy val jvmProjects = Seq[ProjectReference](
+  genJVM, coreJVM, scalapropsJVM, scalazlawsJVM
+)
+
+lazy val jsProjects = Seq[ProjectReference](
+  genJS, coreJS, scalapropsJS, scalazlawsJS
+)
+
+lazy val genJS = gen.js
+lazy val genJVM = gen.jvm
+lazy val genRoot = project.aggregate(genJS, genJVM)
+
+lazy val coreJS = core.js
+lazy val coreJVM = core.jvm
+lazy val coreRoot = project.aggregate(coreJS, coreJVM)
+
+lazy val scalazlawsJS = scalazlaws.js
+lazy val scalazlawsJVM = scalazlaws.jvm
+lazy val scalazlawsRoot = project.aggregate(scalazlawsJS, scalazlawsJVM)
+
+lazy val scalapropsJS = scalaprops.js
+lazy val scalapropsJVM = scalaprops.jvm
+lazy val scalapropsRoot = project.aggregate(scalapropsJS, scalapropsJVM)
+
+val root = Project("root", file(".")).settings(
+  Common.commonSettings ++
+  unidocSettings ++
+  xerial.sbt.Sonatype.sonatypeRootSettings ++ (
+    coreJVM ::
+    scalapropsJVM ::
+    scalazlawsJVM ::
+    Nil
+  ).map(libraryDependencies <++= libraryDependencies in _)
+).settings(
+  name := allName,
+  artifacts := Nil,
+  unidocProjectFilter in (ScalaUnidoc, unidoc) := {
+    jsProjects.foldLeft(inAnyProject)((acc, a) => acc -- inProjects(a))
+  },
+  packagedArtifacts := Map.empty,
+  artifacts <++= Classpaths.artifactDefs(Seq(packageDoc in Compile)),
+  packagedArtifacts <++= Classpaths.packaged(Seq(packageDoc in Compile)),
+  Sxr.settings1,
+  Defaults.packageTaskSettings(
+    packageDoc in Compile, (UnidocKeys.unidoc in Compile).map{_.flatMap(Path.allSubpaths)}
+  ),
+  Sxr.settings2
+).aggregate(
+  jvmProjects ++ jsProjects : _*
+)
+
+lazy val rootJS = project.aggregate(jsProjects: _*)
+lazy val rootJVM = project.aggregate(jvmProjects: _*)
--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,11 @@ import org.scalajs.sbtplugin.cross.{CrossType, CrossProject}
 import org.scalajs.sbtplugin.ScalaJSPlugin
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
-object build extends Build {
+object build {
 
   private[this] val genName = "scalaprops-gen"
   private[this] val coreName = "scalaprops-core"
-  private[this] val allName = "scalaprops-all"
+  val allName = "scalaprops-all"
   private[this] val scalazlawsName = "scalaprops-scalazlaws"
   private[this] val scalapropsName = "scalaprops"
 
@@ -66,25 +66,13 @@ object build extends Build {
     Generator.settings: _*
   )
 
-  lazy val genJS = gen.js
-  lazy val genJVM = gen.jvm
-  lazy val genRoot = project.aggregate(genJS, genJVM)
-
   lazy val core = module("core").settings(
     name := coreName
   ).dependsOn(gen)
 
-  lazy val coreJS = core.js
-  lazy val coreJVM = core.jvm
-  lazy val coreRoot = project.aggregate(coreJS, coreJVM)
-
   lazy val scalazlaws = module("scalazlaws").settings(
     name := scalazlawsName
   ).dependsOn(core)
-
-  lazy val scalazlawsJS = scalazlaws.js
-  lazy val scalazlawsJVM = scalazlaws.jvm
-  lazy val scalazlawsRoot = project.aggregate(scalazlawsJS, scalazlawsJVM)
 
   lazy val scalaprops = module(scalapropsName).settings(
     name := scalapropsName
@@ -97,46 +85,4 @@ object build extends Build {
     libraryDependencies += "org.scala-js" %% "scalajs-test-interface" % scalaJSVersion
   )
 
-  lazy val scalapropsJS = scalaprops.js
-  lazy val scalapropsJVM = scalaprops.jvm
-  lazy val scalapropsRoot = project.aggregate(scalapropsJS, scalapropsJVM)
-
-  import UnidocKeys._
-
-  private[this] lazy val jvmProjects = Seq[ProjectReference](
-    genJVM, coreJVM, scalapropsJVM, scalazlawsJVM
-  )
-  private[this] lazy val jsProjects = Seq[ProjectReference](
-    genJS, coreJS, scalapropsJS, scalazlawsJS
-  )
-
-  val root = Project("root", file(".")).settings(
-    commonSettings ++
-    unidocSettings ++
-    xerial.sbt.Sonatype.sonatypeRootSettings ++ (
-      coreJVM ::
-      scalapropsJVM ::
-      scalazlawsJVM ::
-      Nil
-    ).map(libraryDependencies <++= libraryDependencies in _)
-  ).settings(
-    name := allName,
-    artifacts := Nil,
-    unidocProjectFilter in (ScalaUnidoc, unidoc) := {
-      jsProjects.foldLeft(inAnyProject)((acc, a) => acc -- inProjects(a))
-    },
-    packagedArtifacts := Map.empty,
-    artifacts <++= Classpaths.artifactDefs(Seq(packageDoc in Compile)),
-    packagedArtifacts <++= Classpaths.packaged(Seq(packageDoc in Compile)),
-    Sxr.settings1,
-    Defaults.packageTaskSettings(
-      packageDoc in Compile, (UnidocKeys.unidoc in Compile).map{_.flatMap(Path.allSubpaths)}
-    ),
-    Sxr.settings2
-  ).aggregate(
-    jvmProjects ++ jsProjects : _*
-  )
-
-  lazy val rootJS = project.aggregate(jsProjects: _*)
-  lazy val rootJVM = project.aggregate(jvmProjects: _*)
 }
