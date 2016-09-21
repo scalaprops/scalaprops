77d3cc5e4f29f157c45f716f77d95f8172a8ee69
xuwei-k <6b656e6a69@gmail.com>
2016-09-21 10:53:59 +0900

    use build.sbt
    - https://github.com/sbt/sbt/commit/f2f12022a5672c3f57b8e5fce4227da455ab8024
    - https://github.com/sbt/sbt/blob/v0.13.12/main/src/main/scala/sbt/Build.scala#L11

---
+++ build.sbt
@@ -0,0 +1,53 @@
+import sbtunidoc.Plugin._
+import build._
+import Common._
+import UnidocKeys._
+
+lazy val gen = module("gen").settings(
+  Generator.settings
+).settings(
+  name := genName,
+  description := "pure functional random value generator",
+  libraryDependencies += "org.scalaz" %% "scalaz-core" % scalazVersion.value
+)
+
+lazy val core = module("core").settings(
+  name := coreName
+).dependsOn(gen)
+
+lazy val scalazlaws = module("scalazlaws").settings(
+  name := scalazlawsName
+).dependsOn(core)
+
+lazy val scalaprops = module(scalapropsName).settings(
+  name := scalapropsName,
+  libraryDependencies += "org.scala-sbt" % "test-interface" % "1.0"
+).dependsOn(core, scalazlaws % "test")
+
+
+val root = Project("root", file(".")).settings(
+  commonSettings ++
+  unidocSettings ++
+  xerial.sbt.Sonatype.sonatypeRootSettings ++ (
+    core ::
+    scalaprops ::
+    scalazlaws ::
+    Nil
+  ).map(libraryDependencies <++= libraryDependencies in _)
+).settings(
+  name := allName,
+  artifacts := Nil,
+  packagedArtifacts := Map.empty,
+  artifacts <++= Classpaths.artifactDefs(Seq(packageDoc in Compile)),
+  packagedArtifacts <++= Classpaths.packaged(Seq(packageDoc in Compile))
+).settings(
+  Sxr.settings1
+).settings(
+  Defaults.packageTaskSettings(
+    packageDoc in Compile, (UnidocKeys.unidoc in Compile).map{_.flatMap(Path.allSubpaths)}
+  )
+).settings(
+  Sxr.settings2
+).aggregate(
+  gen, core, scalaprops, scalazlaws
+)
--- project/Build.scala
+++ project/Build.scala
@@ -1,14 +1,13 @@
 import sbt._, Keys._
-import sbtunidoc.Plugin._
 import Common._
 
-object build extends Build {
+object build {
 
-  private[this] val genName = "scalaprops-gen"
-  private[this] val coreName = "scalaprops-core"
-  private[this] val allName = "scalaprops-all"
-  private[this] val scalazlawsName = "scalaprops-scalazlaws"
-  private[this] val scalapropsName = "scalaprops"
+  val genName = "scalaprops-gen"
+  val coreName = "scalaprops-core"
+  val allName = "scalaprops-all"
+  val scalazlawsName = "scalaprops-scalazlaws"
+  val scalapropsName = "scalaprops"
 
   val scalazVersion = SettingKey[String]("scalazVersion")
 
@@ -21,7 +20,7 @@ object build extends Build {
     Nil
   )
 
-  private[this] def module(id: String) =
+  def module(id: String) =
     Project(id, file(id)).settings(commonSettings).settings(
       scalazVersion := "7.1.10",
       initialCommands in console += {
@@ -30,55 +29,4 @@ object build extends Build {
         ).map(a => s"val $a = scalaprops.$a").mkString(";") // for tab completion
       }
     )
-
-  lazy val gen = module("gen").settings(
-    Generator.settings
-  ).settings(
-    name := genName,
-    description := "pure functional random value generator",
-    libraryDependencies += "org.scalaz" %% "scalaz-core" % scalazVersion.value
-  )
-
-  lazy val core = module("core").settings(
-    name := coreName
-  ).dependsOn(gen)
-
-  lazy val scalazlaws = module("scalazlaws").settings(
-    name := scalazlawsName
-  ).dependsOn(core)
-
-  lazy val scalaprops = module(scalapropsName).settings(
-    name := scalapropsName,
-    libraryDependencies += "org.scala-sbt" % "test-interface" % "1.0"
-  ).dependsOn(core, scalazlaws % "test")
-
-  import UnidocKeys._
-
-  val root = Project("root", file(".")).settings(
-    commonSettings ++
-    unidocSettings ++
-    xerial.sbt.Sonatype.sonatypeRootSettings ++ (
-      core ::
-      scalaprops ::
-      scalazlaws ::
-      Nil
-    ).map(libraryDependencies <++= libraryDependencies in _)
-  ).settings(
-    name := allName,
-    artifacts := Nil,
-    packagedArtifacts := Map.empty,
-    artifacts <++= Classpaths.artifactDefs(Seq(packageDoc in Compile)),
-    packagedArtifacts <++= Classpaths.packaged(Seq(packageDoc in Compile))
-  ).settings(
-    Sxr.settings1
-  ).settings(
-    Defaults.packageTaskSettings(
-      packageDoc in Compile, (UnidocKeys.unidoc in Compile).map{_.flatMap(Path.allSubpaths)}
-    )
-  ).settings(
-    Sxr.settings2
-  ).aggregate(
-    gen, core, scalaprops, scalazlaws
-  )
-
 }
