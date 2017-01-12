0c08705015a0861944a224b0f6e0cac99fa0d404
kenji yoshida <6b656e6a69@gmail.com>
2017-01-12 09:29:07 +0900

    decrease CofreeTest.zipIList param

--- scalaprops/src/test/scala/scalaprops/CofreeTest.scala
+++ scalaprops/src/test/scala/scalaprops/CofreeTest.scala
@@ -48,7 +48,7 @@ object CofreeTest extends Scalaprops {
   val zipIList = {
     import CofreeGenImplicit._
     cofreeZipTest[IList]
-  }.andThenParam(Param.maxSize(2) andThen Param.minSuccessful(5))
+  }.andThenParam(Param.maxSize(2) andThen Param.minSuccessful(2))
 
   val zipValidation = {
     import CofreeGenImplicit._
