0e8e2c5100c77ec6442764b7ab7d393fb75c6c22
kenji yoshida <6b656e6a69@gmail.com>
2017-01-17 15:47:49 +0900

    decrease param count A001791 test

--- scalaprops/jvm/src/test/scala/scalaprops/TreeLocTestJVM.scala
+++ scalaprops/jvm/src/test/scala/scalaprops/TreeLocTestJVM.scala
@@ -7,7 +7,7 @@ object TreeLocTestJVM extends Scalaprops {
     * @see [[https://oeis.org/A001791]]
     */
   val A001791 = TreeTest.sizeTest(
-    List(1, 4, 15, 56, 210),
+    List(1, 4, 15, 56),
     (i, seed) => Gen.treeLocGenSized[Unit](i).infiniteStream(seed = seed)
   )
 }
