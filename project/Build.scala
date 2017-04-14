8657e4683d1b6ea20b245d000696f0b6e9a6d333
kenji yoshida <6b656e6a69@gmail.com>
2017-04-14 18:42:22 +0900

    scalaz 7.2.11

--- project/Build.scala
+++ project/Build.scala
@@ -40,7 +40,7 @@ object build {
   private[this] def module(id: String) =
     CrossProject(id, file(id), CustomCrossType).settings(
       commonSettings,
-      scalazVersion := "7.2.10",
+      scalazVersion := "7.2.11",
       scalaJSStage in Test := FastOptStage,
       jsEnv := NodeJSEnv().value,
       initialCommands in console += {
