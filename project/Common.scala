8fb292a7076cf649c798d0843faa5a781ec3574e
xuwei-k <6b656e6a69@gmail.com>
2017-05-03 11:30:29 +0900

    Scala 2.13.0-M1

--- project/Common.scala
+++ project/Common.scala
@@ -27,7 +27,7 @@ object Common {
 
   val commonSettings = scalaprops.ScalapropsPlugin.autoImport.scalapropsCoreSettings ++ Seq(
     scalaVersion := Scala211,
-    crossScalaVersions := "2.12.2" :: Scala211 :: "2.10.6" :: Nil,
+    crossScalaVersions := "2.13.0-M1" :: "2.12.2" :: Scala211 :: "2.10.6" :: Nil,
     organization := "com.github.scalaprops",
     description := "property based testing library for Scala",
     fullResolvers ~= {_.filterNot(_.name == "jcenter")},
