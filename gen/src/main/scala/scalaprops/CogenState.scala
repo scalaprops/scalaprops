bdf8786fdffd675c37f8d6bd210baec7556d333d
xuwei-k <6b656e6a69@gmail.com>
2017-05-28 13:53:56 +0900

    add Functor[CogenState]

--- gen/src/main/scala/scalaprops/CogenState.scala
+++ gen/src/main/scala/scalaprops/CogenState.scala
@@ -1,3 +1,16 @@
 package scalaprops
 
-final case class CogenState[A](rand: Rand, gen: Gen[A])
+import scalaz.Functor
+
+final case class CogenState[A](rand: Rand, gen: Gen[A]) {
+  def map[B](f: A => B): CogenState[B] =
+    CogenState(rand, gen.map(f))
+}
+
+object CogenState {
+  implicit val instance: Functor[CogenState] =
+    new Functor[CogenState] {
+      override def map[A, B](fa: CogenState[A])(f: A => B) =
+        fa map f
+    }
+}
