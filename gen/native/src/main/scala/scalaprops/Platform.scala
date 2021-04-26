cda84c2ff38e2f181a92304f95bcbe93dfa2be84
kenji yoshida <6b656e6a69@gmail.com>
2021-04-26 12:30:32 +0900

    change scala-native default `size` and `minSuccessful`

--- gen/native/src/main/scala/scalaprops/Platform.scala
+++ gen/native/src/main/scala/scalaprops/Platform.scala
@@ -1,9 +1,9 @@
 package scalaprops
 
 private[scalaprops] object Platform {
-  def minSuccessful = 100
+  def minSuccessful = 50
   def randFromLong(seed: Long) = MersenneTwister32.standard(seed)
   def randFromInt(seed: Int) = MersenneTwister32.standard(seed)
-  def genSize = 100
+  def genSize = 50
   def className[A](clazz: Class[A]) = clazz.getSimpleName
 }
