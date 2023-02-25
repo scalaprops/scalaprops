0db3cb500531141e8f3ed9f53fae481d3c2c3923
kenji yoshida <6b656e6a69@gmail.com>
2023-02-25 09:08:16 +0900

    add explicit companion objects for Scala 3

--- core/src/main/scala/scalaprops/Seed.scala
+++ core/src/main/scala/scalaprops/Seed.scala
@@ -1,5 +1,7 @@
 package scalaprops
 
+import scala.runtime.AbstractFunction1
+
 abstract class Seed extends Product with Serializable {
   def createRand: Rand
 }
@@ -8,10 +10,13 @@ object Seed {
   final case class LongSeed(value: Long) extends Seed {
     override def createRand: Rand = Platform.randFromLong(value)
   }
+  object LongSeed extends AbstractFunction1[Long, LongSeed]
   final case class IntSeed(value: Int) extends Seed {
     override def createRand: Rand = Platform.randFromInt(value)
   }
+  object IntSeed extends AbstractFunction1[Int, IntSeed]
   final case class RandSeed(value: Rand) extends Seed {
     override def createRand: Rand = value
   }
+  object RandSeed extends AbstractFunction1[Rand, RandSeed]
 }
