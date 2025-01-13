89451097fdae94ea9f40b24ec44ef203516f0c46
kenji yoshida <6b656e6a69@gmail.com>
2025-01-13 12:52:42 +0900

    update UpdateReadme.scala

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -5,7 +5,7 @@ import sbtrelease.Git
 object UpdateReadme {
   private val sonatypeURL = "https://oss.sonatype.org/service/local/repositories/"
 
-  val updateReadmeTask = { state: State =>
+  val updateReadmeTask = { (state: State) =>
     val extracted = Project.extract(state)
     val scalaV = "2.12"
     val v = extracted get version
