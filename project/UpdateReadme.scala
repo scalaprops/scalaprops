f2c8cb2d13de4bc0564c21b09c4593c0c67fbb53
xuwei-k <6b656e6a69@gmail.com>
2025-11-03 10:17:54 +0900

    delete scaladoc link

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -4,15 +4,11 @@ import sbtrelease.Git
 import sbtrelease.ReleasePlugin.autoImport.ReleaseStep
 
 object UpdateReadme {
-  private val sonatypeURL = "https://oss.sonatype.org/service/local/repositories/"
-
   val updateReadmeTask = { (state: State) =>
     val extracted = Project.extract(state)
-    val scalaV = "2.12"
     val v = extracted get version
     val org = extracted get organization
     val modules = build.modules
-    val snapshotOrRelease = if (extracted get isSnapshot) "snapshots" else "releases"
     val readme = "README.md"
     val readmeFile = file(readme)
     val newReadme = Predef
@@ -23,14 +19,6 @@ object UpdateReadme {
         if (line.startsWith("libraryDependencies") && matchReleaseOrSnapshot) {
           val i = modules.map("\"" + _ + "\"").indexWhere(line.contains)
           s"""libraryDependencies += "$org" %% "${modules(i)}" % "$v" % "test""""
-        } else if (line.contains(sonatypeURL) && matchReleaseOrSnapshot) {
-          val n = extracted.get(LocalRootProject / name)
-          val javadocIndexHtml = "-javadoc.jar/!/scalaprops/index.html"
-          val baseURL =
-            s"${sonatypeURL}${snapshotOrRelease}/archive/${org.replace('.', '/')}/${n}_${scalaV}/${v}/${n}_${scalaV}-${v}"
-          if (line.contains(javadocIndexHtml)) {
-            s"- [API Documentation](${baseURL}${javadocIndexHtml})"
-          } else line
         } else line
       }
       .mkString("", "\n", "\n")
